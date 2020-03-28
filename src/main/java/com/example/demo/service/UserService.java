38dbc8c477f744c037eeb7050c93f46ac904c88e
DianaCrainic <dianacrainic801@yahoo.com>
2020-03-28 15:50:07 +0200

    Small fix.

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,7 +5,9 @@ import com.example.demo.UserRepository;
 import com.example.demo.dto.UserDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
